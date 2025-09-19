71053a13848758875ecd2fbf08c1769160ad472f
Okeanalysis <emmanueloke760@gmail.com>
2025-09-19 20:53:53 +0100

    day 2

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,7 +3,7 @@ bill= float(input("what is the bill: "))
 tip=int(input("how much tip will you like to give? 10, 12, 15: "))
 people= int(input("how many people are we spliting the bill among: "))
 tip_percent= 1+(tip/100)
-totalbill= bill+tip_percent
+totalbill= bill*tip_percent
 totalbillp= totalbill/people
 totalamount= round(totalbillp, 2)
 
