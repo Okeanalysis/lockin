f3db2bdadaca77fda4a0c1ac3ac31bc2be4f9c79
Okeanalysis <emmanueloke760@gmail.com>
2025-09-20 21:42:33 +0100

    day3

---
+++ Day3/leap.py
@@ -0,0 +1,13 @@
+leap= int(input("enter the year:"))
+
+
+if leap%4==0:
+    if leap%100==0:
+        if leap%400==0:
+            print(f"{leap} is a leap year")
+        else:
+            print(f"{leap} is not a leap year")
+    else:
+        print(f"{leap} is not a leap year")
+else:
+    print(f"{leap} is not a leap year")
