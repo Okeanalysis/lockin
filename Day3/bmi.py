71cbcf80a82f7f592c2c8c294489950e151a8681
Okeanalysis <emmanueloke760@gmail.com>
2025-09-20 21:35:05 +0100

    day3

--- Day3/bmi.py
+++ Day3/bmi.py
@@ -5,11 +5,11 @@ bmi= round(weight/height**2)
 
 if bmi < 18.5:
     print("your bmi is",bmi,"your are underweight")
-elif bmi >18.5 and bmi <25:
+elif bmi <25:
     print("your bmi is",bmi,"you have a normal weight")
-elif bmi >25 and bmi < 30:
+elif bmi < 30:
     print("your bmi is",bmi,"your are overweight")
-elif bmi >30 and bmi <35:
+elif bmi <35:
     print("your bmi is",bmi,"your are obese")
 else:
     print("your bmi is",bmi,"your are clinically obese")
