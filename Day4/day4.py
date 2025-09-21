7dfdc0c8c084e4b4557d31dd0810ea9c2c0bb8ba
Okeanalysis <emmanueloke760@gmail.com>
2025-09-21 18:55:07 +0100

    day4

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,4 +1,10 @@
+import random
 names= input("Give me the name of the people: ")
 
-name= names.split(", ")
-print(name)
+name= names.split(',')
+num= len(name)
+
+
+randoms= random.randint(0, num-1)
+pay= name[randoms]
+print(f"{pay} is paying for the food")
