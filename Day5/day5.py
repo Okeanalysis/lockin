86cb7fd072213028831f4b5b30acdd836e02eba6
Okeanalysis <emmanueloke760@gmail.com>
2025-09-22 22:55:03 +0100

    day5

---
+++ Day5/day5.py
@@ -0,0 +1,8 @@
+import random
+alp= ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+num= ['1', '2', '3', '4', '5', '6', '7', '9']
+symbol= []
+print("Welcome to the pypassword generator")
+num1 = int(input("How many alphabet do you wnat in your password: "))
+alpha= int(input("how many symbols do you want in your password: "))
+sym=int(input("how many numbers do you want in your password: "))
